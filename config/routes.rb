8b865ead048bee79ffd78f296ae57608a5c9f98d
jean luc Gaman <gaman2404@gmail.com>
2019-08-15 17:35:04 +0200

    controller cocktail et doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:index, :show, :new, :delete]
+    resources :doses, only: [:new, :create]
   end
+
+  resources :doses, only: [:destroy]
 end
