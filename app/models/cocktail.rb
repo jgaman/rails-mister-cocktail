4a4c2426d1bdc44876acf4e66416b8ac811b77f1
jean luc Gaman <gaman2404@gmail.com>
2019-08-15 14:30:40 +0200

    Validate the models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  # validates :category, inclusion: { in: CATEGORIES }
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
 end
